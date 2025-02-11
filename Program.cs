0d83b6376b65298be68310da5ae620a26dcdf0b1
peter723 <petermelad404@gmail.com>
2025-02-11 14:09:50 +0200

    add server sql ip

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-
 using Hr_System_Demo_3.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -15,51 +14,56 @@ namespace Hr_System_Demo_3
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
+            // Add services to the container
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            // Database Configuration
+            var connectionString = builder.Environment.IsDevelopment()
+                ? "Data Source=82.112.254.244,1433;Initial Catalog=Hr_System_DB;Persist Security Info=True;Encrypt=false;TrustServerCertificate=true;User ID=sa;Password=Peter@123"
+                : "Data Source=172.17.0.12,1433;Initial Catalog=Hr_System_DB;Persist Security Info=True;Encrypt=false;TrustServerCertificate=true;User ID=sa;Password=Peter@123";
 
-            builder.Services.AddDbContext<AppDbContext>(builder => builder.UseSqlServer("Data Source=82.112.254.244,1433;Initial Catalog=Hr_System_DB;Persist Security Info=True;Encrypt=false;TrustServerCertificate=true;User ID=sa;Password=Peter@123"));
-           // builder.Services.AddDbContext<AppDbContext>(builder => builder.UseSqlServer("Data Source=SHADY\\SQLEXPRESS; database = Hr_System_Demo_3;Integrated Security=True; trust server certificate = true"));
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 
-            var jwtOptions = builder.Configuration.GetSection("Jwt").Get<JwtOptions>();
+            // Configure JWT Authentication
+            var jwtSection = builder.Configuration.GetSection("Jwt");
+            builder.Services.Configure<JwtOptions>(jwtSection);
+
+            var jwtOptions = jwtSection.Get<JwtOptions>();
+            if (jwtOptions == null || string.IsNullOrEmpty(jwtOptions.SigningKey))
+            {
+                throw new Exception("JWT configuration is missing or invalid.");
+            }
+
             builder.Services.AddSingleton(jwtOptions);
 
             builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-     .AddJwtBearer(options =>
-     {
-         options.TokenValidationParameters = new TokenValidationParameters
-         {
-             ValidateIssuer = true,
-             ValidIssuer = jwtOptions.Issuer,
-             ValidateAudience = true,
-             ValidAudience = jwtOptions.Audience,
-             ValidateIssuerSigningKey = true,
-             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SigningKey)),
-             RoleClaimType = ClaimTypes.Role 
-         };
-     });
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = jwtOptions.Issuer,
+                        ValidateAudience = true,
+                        ValidAudience = jwtOptions.Audience,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SigningKey)),
+                        RoleClaimType = ClaimTypes.Role
+                    };
+                });
 
             var app = builder.Build();
 
-            //// Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            //{
-                app.MapOpenApi();
-               // app.UseSwaggerUI(options=> options.SwaggerEndpoint("/openapi/v1.json","auth"));
-                app.MapScalarApiReference();
-            //}
-
+            // Configure Middleware
             app.UseHttpsRedirection();
-
             app.UseAuthentication();
             app.UseAuthorization();
 
-
             app.MapControllers();
+
+            // OpenAPI / Swagger
+            app.MapOpenApi();
+            app.MapScalarApiReference();
 
             app.Run();
         }
